ebe35d63fbf7a3ee5c42861a4894bdffbf385112
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2023-09-16 22:36:39 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "Dudul", quotes: "Apapun Yang Terjadi, Tetaplah Bernafas Ya Gigis" },
-    { author: "Dudul", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "Dudul", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "Dudul", quotes: "I love you." },
-    { author: "Dudul", quotes: "Mungkin Aku Tidak Bertemu Dengan Mu Tiap Hari, Tapi Aku Mencintai Mu Setiap Hari, I love you gigis<3." },
-    { author: "Dudul", quotes: "I love you." },
-    { author: "Dudul", quotes: "I'm sorry for loving you." },
+    { author: "From Dudul", quotes: "Apapun Yang Terjadi, Tetaplah Bernafas Ya Gigis" },
+    { author: "From Dudul", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "From Dudul", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "From Dudul", quotes: "I love you." },
+    { author: "From Dudul", quotes: "Mungkin Aku Tidak Bertemu Dengan Mu Tiap Hari, Tapi Aku Mencintai Mu Setiap Hari, I love you gigis<3." },
+    { author: "From Dudul", quotes: "I love you." },
+    { author: "From Dudul", quotes: "I'm sorry for loving you." },
 ];
