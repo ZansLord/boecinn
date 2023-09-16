588e1672e3f679ce93331b45b98cfc972700b1b9
ZansLord <shikaizaidan@gmail.com>
2023-09-16 22:29:57 +0700

    i love you gisya

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Gisya", "Giss", "Gigissss", "Gisyaaaaa"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
